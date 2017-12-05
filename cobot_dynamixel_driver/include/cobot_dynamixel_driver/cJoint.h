39b0fe82d2ac8d4aed902f4fe9fbdc671f5cbd7e
tong <anusorni@mtec.or.th>
2017-12-06 00:18:17 +0700

    ...

--- cobot_dynamixel_driver/include/cobot_dynamixel_driver/cJoint.h
+++ cobot_dynamixel_driver/include/cobot_dynamixel_driver/cJoint.h
@@ -12,27 +12,32 @@
 class cJoint{
 private:
   int id, goal_pos, goal_velo, goal_torque;
-  int pos, velo, load;
+  int pos, velo, current, input_voltage, temperature;
+  double rad2val, velo2val;
   std::string name;
   
   // xml
   std::string motor_name;
   int motor_model_number;
-  double cw_angle_limit, ccw_angle_limit, torque_limit, velo_limit, acc_limit, gear_ratio;
+  double cw_angle_limit, ccw_angle_limit, torque_limit, velocity_limit, acceleration_limit
+  , gear_ratio, position_value, current_max;
 
   static dynamixel::PacketHandler *packetHandler;
   static dynamixel::PortHandler *portHandler;
   static dynamixel::GroupSyncWrite *group_write_velo, *group_write_pos_velo;
-  static dynamixel::GroupBulkRead *group_read;
+  static dynamixel::GroupSyncRead *group_read;
   static std::vector<cJoint> joints;
   static int mode;
   
 
 private:
-  void write1b(int addr, int val);
+  void write(const int param, const int val);
+  int read(const int param);
+  void setup();
+/*  void write1b(int addr, int val);
   void write2b(int addr, int val);
   int read1b(int addr);
-  int read2b(int addr);
+  int read2b(int addr);*/
 
   static void load_settings();
 
@@ -47,7 +52,8 @@ public:
   inline const std::string& get_name() const { return name; }
   double get_pos() const;
   double get_velo() const;
-  double get_load() const;
+  double get_current() const; // mA
+  double get_load() const;    // current / max_current
   
   
   static std::vector<cJoint> &init();
