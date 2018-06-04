cb4f178f3710249c46e78e3b17034a4163ab9a92
Tong <tongttt.and@gmail.com>
2018-06-04 22:45:53 +0700

    ...

--- cobot_dynamixel_driver/script/calibrate.py
+++ cobot_dynamixel_driver/script/calibrate.py
@@ -18,45 +18,50 @@ from moveit_msgs.srv import GetPositionFK
 fname = 'pos_calib.txt'
 
 compute_fk = None
-def find_err(pos, dq, dqmax):
+def find_err(pos, off, dq, dqmax):
   global compute_fk
   header = Header()
   header.frame_id = 'base_link'
   robot_state = RobotState()
-  robot_state.header = header
   joint_names = [('J'+str(i+1)) for i in range(6)]
   robot_state.joint_state.name = joint_names
-
+  robot_state.joint_state.position = [0]*6
+  rb_pos = robot_state.joint_state.position
   best_score = 999999999.0
-  n_loop = (dqmax*2 / dq)**6
-  q = np.array([0]*6)
-  for q1 in range(n):
-    q[0] = dq*q1 - dqmax
-    for q2 in range(n):
-      q[1] = dq*q2 - dqmax
-      for q3 in range(n):
-        q[2] = dq*q3 - dqmax
-        for q4 in range(n):
-          q[3] = dq*q4 - dqmax
-          for q5 in range(n):
-            q[4] = dq*q5 - dqmax
-            for q6 in range(n):
-              q[5] = dq*q6 - dqmax
-              score = 0
-              for j in range(pos):
-                pos2 = []
-                for k in range(pos[j]):
-                  for l in range(len(pos))
-                  c = compute_fk(header, ['tool0'], robot_state)
-                  if c.error_code.val==1:
-                    p = c.pose_stamped[0].pose.position
-                    pos2.append([p.x,p.y,p.z])
-                pos2 = np.array(pos2)
-                score+= np.var(pos2[:,0]) + np.var(pos2[:,1]) + np.var(pos2[:,2])
-              if score < best_score:
-                best_score = score
-                best_q = copy.deepcopy(q)
-                print(str(best_score) + ' : ' + str(best_q))
+  n = dqmax*2 / dq
+  q = np.array(off)
+  with open('calib_result.txt', 'at') as f:
+    for q1 in range(n):
+      print('q1 : ' + str(q1))
+      q[0] = dq*q1 - dqmax
+      for q2 in range(n):
+        q[1] = dq*q2 - dqmax
+        for q3 in range(n):
+          q[2] = dq*q3 - dqmax
+          for q4 in range(n):
+            q[3] = dq*q4 - dqmax
+            for q5 in range(n):
+              q[4] = dq*q5 - dqmax
+              for q6 in range(n):
+                q[5] = dq*q6 - dqmax
+                score = 0
+                for j in range(pos):
+                  pos2 = []
+                  for k in range(pos[j]):
+                    for l in range(6):
+                      rb_pos[l] = pos[j][k][l] + q[l]
+                    c = compute_fk(header, ['tool0'], robot_state)
+                    if c.error_code.val==1:
+                      p = c.pose_stamped[0].pose.position
+                      pos2.append([p.x,p.y,p.z])
+                  pos2 = np.array(pos2)
+                  score+= np.var(pos2[:,0]) + np.var(pos2[:,1]) + np.var(pos2[:,2])
+                if score < best_score:
+                  best_score = score
+                  best_q = copy.deepcopy(q)
+                  print(str(best_score) + ' : ' + str(best_q))
+                  f.write('%f %f %f %f %f %f %f\n', best_score \
+                    , q[0], q[1], q[2], q[3], q[4], q[5])
   return best_q
 
 if __name__ == "__main__":
@@ -83,7 +88,8 @@ if __name__ == "__main__":
     #print(pos)
     rospy.wait_for_service('compute_fk')
     compute_fk = rospy.ServiceProxy('compute_fk', GetPositionFK)
-    find_err(pos, 0.2, 3.0)
-    c = compute_fk(header, ['tool0'], robot_state)
+    off = find_err(pos, [0]*6, 0.5, 5.0)
+    find_err(pos, off, 0.1, 1.0)
+    print(off)
   except KeyboardInterrupt:
     print('SIGINT')
