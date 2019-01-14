60f4e95b0fd4e236293bebdbe63d116dd812117e
tong <tongttt.and@gmail.com>
2019-01-15 06:05:45 +0700

    ...

--- cobot_planner/scripts/find_current2torque_static/find_current2torque_static.py
+++ cobot_planner/scripts/find_current2torque_static/find_current2torque_static.py
@@ -22,6 +22,7 @@ import sys
 import os
 import math
 import numpy as np
+from numpy import linalg
 import matplotlib.pyplot as plt
 
 import eq
@@ -55,6 +56,7 @@ class LSM:
     n = len(x)
 
     div = n*xx - nx**2
+    assert(div!=0)
     a = (n*xy - nx*ny)/div
     b = (xx*ny - xy*nx)/div
     return a,b
@@ -66,23 +68,117 @@ class LSM:
   def get_y(self,x,a,b):
     return a*x+b
 
+
+class cFindEq:
+  def __init__(self):
+    self.bias = None
+    self.y_est = None
+    
+  def set_data( self, x, y, ab ):
+    self.x = x
+    self.y = y
+    self.ab = ab
+  
+  
+  def get_bias(self, data):
+    if self.bias is not None:
+      return self.bias
+      
+    self.bias = np.zeros(len(data))
+    for i in range(len(data)):
+      if data[i]>0.0:
+        self.bias[i] = 1.0
+      elif data[i]<0.0:
+        self.bias[i] = -1.0
+      else:
+        self.bias[i] = 0.0
+    return self.bias
+
+
+  def find_range_ab(self):
+    xy = [[],[]]
+    for i in range(len(self.x)):
+      if self.y[i]>0.0:
+        xy[0].append([self.x[i], self.y[i]])
+      elif self.y[i]<0.0:
+        xy[1].append([-self.x[i], -self.y[i]])
+    lsm = LSM()
+    ab = [None, None]
+    for i in range(2):
+      if len(xy[i])>3:
+        xy2 = np.array(xy[i])
+        ab[i] = np.array( lsm.fit( xy2[:,0], xy2[:,1] ) )
+    if ab[0] is not None and ab[1] is not None:
+      mean = 0.5*(ab[0] + ab[1])
+      diff = abs(ab[0] - ab[1])
+    else:
+      if ab[0] is None:
+        ab = ab[1]
+      else:
+        ab = ab[0]
+      mean = ab
+      diff = abs( ab*1.5 )
+    range_ab = []
+    for i in range(2):
+      range_ab.append([mean[i]-diff[i], mean[i]+diff[i]])
+    print(mean)
+    print(diff)
+    print(range_ab)
+#    exit()
+    return range_ab
+      
+    
+  def run( self, x, y, n_loop ):
+    self.x = x
+    self.y = y
+    bias = self.get_bias(x)
+    range_ab = self.find_range_ab()
+    
+    self.ab = []
+    dab = []
+    min_err = 99999999.0
+    for i in range(2):
+      dab.append( (range_ab[i][1] - range_ab[i][0])/float(n_loop) )
+
+    for i in range(n_loop):
+      a = range_ab[0][0] + dab[0]*i
+      for j in range(n_loop):
+        b = range_ab[1][0] + dab[1]*j
+        y2 = a*x + b*bias
+        e = linalg.norm(y2-y)
+        if e<min_err:
+          min_err = e
+          self.ab = [a,b]
+          print('['+str((i*(n_loop) + j)/float(n_loop**2))+'] '+str(e/len(x))+' : '+str(self.ab))
+    return self.ab
+
+  def get_est_y(self):
+    if self.y_est is None:
+      self.y_est = self.ab[0] * self.x + self.ab[1] * self.get_bias(self.x)
+    return self.y_est
+    
+  def get_error(self):
+    return linalg.norm( self.get_est_y() - self.y )/len(self.x)
+
+
+
 def get_data(data):
   t = data[:,0]
-  goal_torque_up = data[:,1]
+  goal_effort_up = data[:,1]
   effort_up = data[:,2]
-  goal_torque_down = data[:,3]
+  goal_effort_down = data[:,3]
   effort_down = data[:,4]
   q = data[:,5:11]
   q_rad = q*math.pi/180.0
-  return t, goal_torque_up, effort_up, goal_torque_down, effort_down, q, q_rad
+  return t, goal_effort_up, effort_up, goal_effort_down, effort_down, q, q_rad
 
 
-def find_equation(name, n_joint):
+def find_equation(name, n_joint, n_loop):
   data = load(fname)
   if data.shape[0]==0:
     print('No data found')
     return
-  t, goal_torque_up, effort_up, goal_torque_down, effort_down, q, q_rad = get_data(data)
+  t, goal_effort_up, effort_up, goal_effort_down, effort_down, q, q_rad = get_data(data)
   effort_mean = (effort_up + effort_down)*0.5
   effort_fric = abs((effort_up - effort_down)*0.5)
 
@@ -98,6 +194,7 @@ def find_equation(name, n_joint):
   torque_cal = np.array(torque_cal)
 
   # cal current 2 torque
+  '''
   lsm = LSM()
   a,b = lsm.fit(effort_mean, torque_cal[:,n_joint])
   err = lsm.get_error(effort_mean, torque_cal[:,n_joint], a, b)
@@ -105,8 +202,15 @@ def find_equation(name, n_joint):
   torque_fric = a*effort_fric + b
   print('current2torque : a = %f, b = %f, fric = %f' % (a,b,np.mean(torque_fric)))
   print('mean error [N-m]: '+str(err))
+  '''
+  find_eq = cFindEq()
+  a,b = find_eq.run( torque_cal[:,n_joint], effort_mean, n_loop )
+  err = find_eq.get_error()
+  effort_est = find_eq.get_est_y()
+  print('torque2current : a = %f, b = %f, err = %f' % (a,b,err))
 
 
+  '''
   f, axarr = plt.subplots(2, sharex=True)
   for i in range(len(axarr)):
     axarr[i].hold(True)
@@ -123,6 +227,15 @@ def find_equation(name, n_joint):
   axarr[1].set_ylabel('torque [N-m]')
   axarr[len(axarr)-1].set_xlabel('q'+str(n_joint+1)+' [degree]')
   #plt.show()
+  '''
+  f, axarr = plt.subplots(1, sharex=True)
+  axarr.hold(True)
+  axarr.grid(linestyle='-', linewidth='0.5')
+  axarr.plot( q[:,n_joint], effort_up, '*-', q[:,n_joint], effort_down, '*-'
+    , q[:,n_joint], effort_mean, '*-', q[:,n_joint], effort_est, '*-')
+  axarr.legend(['effort_up', 'effort_down', 'effort_mean', 'effort_est'])
+  axarr.set_ylabel('Effort')
+  axarr.set_xlabel('q'+str(n_joint+1)+' [degree]')
   return [a,b]
 
 
@@ -134,7 +247,7 @@ def test_equation(n_joint, ab):
   for k in range(n):
     fname = dir+str(k+1)+'/torque_j'+str(n_joint+1)+'_all.txt'
     data = load(fname)
-    t, goal_torque_up, effort_up, goal_torque_down, effort_down, q, q_rad = get_data(data)
+    t, goal_effort_up, effort_up, goal_effort_down, effort_down, q, q_rad = get_data(data)
     effort_mean = (effort_up + effort_down)*0.5
     effort_fric = abs((effort_up - effort_down)*0.5)
 
@@ -148,7 +261,24 @@ def test_equation(n_joint, ab):
       t1, t2 = cal_torque(q2, dq, ddq, vars)
       torque_cal.append(t1)
     torque_cal = np.array(torque_cal)
+    
+    find_eq = cFindEq()
+    find_eq.set_data( torque_cal[:,n_joint], effort_mean, ab )
+    effort_est = find_eq.get_est_y()
+    err = find_eq.get_error()
+    
+    print('error : %f' % (err))
+    
+    axarr[k].hold(True)
+    axarr[k].grid(linestyle='-', linewidth='0.5')
+    axarr[k].set_ylabel('Effort')
 
+    axarr[k].plot( q[:,n_joint], effort_up, '*-', q[:,n_joint], effort_down, '*-'
+    , q[:,n_joint], effort_mean, '*-', q[:,n_joint], effort_est, '*-')
+
+      
+    # cal torque 2 current
+    '''
     # cal current 2 torque
     lsm = LSM()
     a = ab[0]
@@ -170,12 +300,15 @@ def test_equation(n_joint, ab):
 
   axarr[0].legend(['torque', 'torque_est'])
   axarr[len(axarr)-1].set_xlabel('q'+str(n_joint+1)+' [degree]')
+  '''
+  axarr[0].legend(['effort_up', 'effort_down', 'effort_mean', 'effort_est'])
+  axarr[len(axarr)-1].set_xlabel('q'+str(n_joint+1)+' [degree]')
   
 
 if __name__ == "__main__":
   n_joint = 4
   fname = 'torque_j' + str(n_joint+1) + '/set_1/torque_j'+str(n_joint+1)+'_all.txt'
-  ab = find_equation(fname, n_joint) # find a,b
+  ab = find_equation(fname, n_joint, 100) # find a,b
   test_equation(n_joint, ab) # test a,b with other dataset
   plt.show()
 
--- cobot_planner/src/cobot_test_eq.cpp
+++ cobot_planner/src/cobot_test_eq.cpp
@@ -9,41 +9,26 @@
 #include <string>
 #include <time.h>
 #include <stdlib.h>
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
-int main(){
+double q[6]// = {1.2,3.0,-1.2,0.5,1.9,0.7}
+  , R[6][4][4]
+  , dR_dq[6][6][4][4]
+  , Kw[6][3][6]
+  , dKw_dq[6][6][3][6]
+  , J[6][4][6]
+  , dJ_dq[6][6][4][6]
+  , dz_dq[6][6]
+  , M[6]
+  , I[6][3][3]
+  , g;
 
-  double q[6]// = {1.2,3.0,-1.2,0.5,1.9,0.7}
-    , R[6][4][4]
-    , dR_dq[6][6][4][4]
-    , Kw[6][3][6]
-    , dKw_dq[6][6][3][6]
-    , J[6][4][6]
-    , dJ_dq[6][6][4][6]
-    , dz_dq[6][6]
-    , M[6]
-    , I[6][3][3]
-    , g;
-
-  srand(time(NULL));
-  for(int i=0;i<6;i++){
-    q[i] = 3.14 * 2 * double(rand() - (RAND_MAX/2)) / RAND_MAX;
-  }
-  
-  get_R( q, R );
-  get_dR_dq( q, dR_dq );
-  get_Kw( q, Kw );
-  get_dKw_dq( q, dKw_dq );
-  get_J( q, J );
-  get_dJ_dq( q, dJ_dq );
-  get_dz_dq( q, dz_dq );
-  get_M( M );
-  get_I( I );
-  get_g( g );
-  
-  FILE *fp =fopen( "/home/tong/catkin_ws/src/cobot/cobot_planner/scripts/test_eq_c.py", "wt" );
+void create_test_file(){
+    FILE *fp =fopen( "/home/tong/catkin_ws/src/cobot/cobot_planner/scripts/test_eq_c.py", "wt" );
   if( !fp ){
     printf("cannot open file\n");
-    return -1;
+    return;
   }
   fprintf(fp, "# created by cobot_test_eq.cpp to check eq_c.h calculation\n\nimport numpy as np\nimport eq\n\n");
   
@@ -222,5 +207,125 @@ int main(){
     }
   }
   fclose(fp);
+
+}
+
+
+void cal_torque(const double q[6],const double dq[6],const double ddq[6]
+    , double torque_no_ddq[6], double torque_ddq[6] ){
+  cv::Mat dR[6], dKw[6], dJ[6];
+  cv::Mat m_dJ_dq[6][6], m_J[6], m_dR_dq[6][6], m_dKw_dq[6][6]
+    , m_R[6], m_I[6], m_Kw[6];
+  for(int i=0;i<6;i++){
+    for(int j=0;j<6;j++){
+      m_dJ_dq[i][j] = cv::Mat( 4, 6, CV_64F );
+      memcpy( m_dJ_dq[i][j].data, &dJ_dq[i][j][0][0], sizeof(double)*4*6 );
+
+/*      m_dR_dq[i][j] = cv::Mat( 3, 3, CV_64F );
+      for(int k=0;k<3;k++){
+        memcpy( m_dR_dq[i][j].data, &dR_dq[i][j][k][0], sizeof(double)*3 );
+      }*/
+      m_dR_dq[i][j] = cv::Mat( 6, 6, CV_64F );
+      memcpy( m_dR_dq[i][j].data, &dR_dq[i][j][0][0], sizeof(double)*6*6 );
+      
+      m_dKw_dq[i][j] = cv::Mat( 3, 6, CV_64F );
+      memcpy( m_dKw_dq[i][j].data, &dKw_dq[i][j][0][0], sizeof(double)*3*6 );
+    }
+    m_J[i] = cv::Mat( 4, 6, CV_64F );
+    memcpy( m_J[i].data, &J[i][0][0], sizeof(double)*4*6 );
+
+    m_R[i] = cv::Mat( 3, 3, CV_64F );
+    for(int j=0;j<3;j++)
+      memcpy( m_R[i].data + sizeof(double)*3*j, &R[i][j][0], sizeof(double)*3 );
+      
+    m_I[i] = cv::Mat( 3, 3, CV_64F );
+    memcpy( m_I[i].data, &I[i][0][0], sizeof(double)*3*3 );
+
+    m_Kw[i] = cv::Mat( 3, 6, CV_64F );
+    memcpy( m_Kw[i].data, &Kw[i][0][0], sizeof(double)*3*6 );
+  }
+  
+  cv::Rect roi(0,0,3,3);
+  for(int i=0;i<6;i++){
+    cv::Mat &dr = dR[i], &dk = dKw[i], &dj = dJ[i];
+    for(int j=0;j<6;j++){
+      if( j==0 ){
+        dr = m_dR_dq[i][j]*dq[j];
+        dj = m_dJ_dq[i][j]*dq[j];
+        dk = m_dKw_dq[i][j]*dq[j];
+      }
+      else{
+        dr+= m_dR_dq[i][j]*dq[j];
+        dj+= m_dJ_dq[i][j]*dq[j];
+        dk+= m_dKw_dq[i][j]*dq[j];
+      }
+      m_dR_dq[i][j] = m_dR_dq[i][j](roi);
+    }
+    dr = dr(roi);
+  }
+  
+
+  double dT_dq[6] = {0.0}
+    , dU_dq[6] = {0.0}
+    , dT_ddq[6] = {0.0};
+  cv::Mat K_ddq( 6, 6, CV_64F );
+  cv::Mat m_dq( 6, 1, CV_64F ), m_ddq( 6, 1, CV_64F );
+  memcpy( m_dq.data, dq, sizeof(double)*6 );
+  memcpy( m_ddq.data, ddq, sizeof(double)*6 );
+  
+  for(int i=0;i<6;i++){
+    for(int j=0;j<6;j++){
+      dT_dq[j]+= 0.5 * ( m_dq.t() * (
+        M[i]*( m_dJ_dq[i][j].t() * (m_J[i]) + m_J[i].t() * (m_dJ_dq[i][j]) )
+        + m_dKw_dq[i][j].t() * (m_R[i]) * (m_I[i]) * (m_R[i].t()) * (m_Kw[i])
+        + m_Kw[i].t() * (m_dR_dq[i][j]) * (m_I[i]) * (m_R[i].t()) * (m_Kw[i])
+        + m_Kw[i].t() * (m_R[i]) * (m_I[i]) * (m_dR_dq[i][j].t()) * (m_Kw[i])
+        + m_Kw[i].t() * (m_R[i]) * (m_I[i]) * (m_R[i].t()) * (m_dKw_dq[i][j])
+        ) * (m_dq) )<double>(0,0);
+      dU_dq[j]+= (M[i]*g*dz_dq[i][j]);
+
+    dT_ddq+= (M[i]*( dJ[i].t() * (m_J[i]) * (m_dq) + m_J[i].t() * (dJ[i]) * (m_dq) )
+      + m_dKw[i].t() * (m_R[i]) * (m_I[i]) * (m_R[i].t()) * (m_Kw[i]) * (m_dq)
+      + m_Kw[i].t() * (dR[i]) * (m_I[i]) * (m_R[i].t()) * (m_Kw[i]) * (m_dq)
+      + m_Kw[i].t() * (m_R[i]) * (m_I[i]) * (dR[i].t()) * (m_Kw[i]) * (m_dq)
+      + m_Kw[i].t() * (m_R[i]) * (m_I[i]) * (m_R[i].t()) * (m_dKw[i]) * (m_dq)).at<double>(0,0);
+
+    K_ddq+= (M[i]*(m_J[i].t() * (m_J[i]))
+      + m_Kw[i].t() * (m_R[i]) * (m_I[i]) * (m_R[i].t()) * (m_Kw[i])).at<double>(0,0);
+  
+  cv::Mat K_ddq2 = K_ddq * (m_ddq);
+  for(int i=0;i<6;i++){
+    torque_no_ddq[i] = - (dT_dq[i] - dU_dq[i] - dT_ddq[i]);
+    torque_ddq[i] = K_ddq2.at<double>(0,0) + torque_no_ddq[i];
+  }
+}
+
+int main(){
+
+
+  srand(time(NULL));
+  for(int i=0;i<6;i++){
+    q[i] = 3.14 * 2 * double(rand() - (RAND_MAX/2)) / RAND_MAX;
+  }
+  
+  get_R( q, R );
+  get_dR_dq( q, dR_dq );
+  get_Kw( q, Kw );
+  get_dKw_dq( q, dKw_dq );
+  get_J( q, J );
+  get_dJ_dq( q, dJ_dq );
+  get_dz_dq( q, dz_dq );
+  get_M( M );
+  get_I( I );
+  get_g( g );
+  
+  //create_test_file(); 
+  
+  cv::Mat m_R = cv::Mat( 4, 4, CV_64F );
+  double q[6] = {0.0}, dq[6] = {0.0}, ddq[6] = {0.0}
+    , torque_no_ddq[6], torque_ddq[6];
+  cal_torque( q, dq, ddq, torque_no_ddq, torque_ddq );
+  
+  
   return 0;
 }
